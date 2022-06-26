aef8e22f8c16cec822bbc0f11aa4f960f06e9afb
g4brieloliveira <g4brieloliveira.dev@gmail.com>
2022-06-26 19:28:19 -0300

    feat: add Watch Me repo

---
+++ ReactJS/Chapter I/Challenge/Watch Me/src/components/SideBar.tsx
@@ -0,0 +1,30 @@
+import { Button } from "./Button";
+
+import "../styles/global.scss";
+import "../styles/sidebar.scss";
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
