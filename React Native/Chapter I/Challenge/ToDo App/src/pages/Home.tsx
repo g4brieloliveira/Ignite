2b73f17c240e7fece17e14cb06e817b2d4a80304
g4brieloliveira <g4brieloliveira.dev@gmail.com>
2022-06-26 19:26:04 -0300

    feat: add Todo App repo

---
+++ React Native/Chapter I/Challenge/ToDo App/src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  function handleAddTask(newTaskTitle: string) {
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
+  function handleToggleTaskDone(id: number) {
+    const updatedTasks = tasks.map((task) => ({ ...task }));
+
+    const foundItem = updatedTasks.find((item) => item.id === id);
+    if (!foundItem) return;
+
+    foundItem.done = !foundItem.done;
+    setTasks(updatedTasks);
+  }
+
+  function handleRemoveTask(id: number) {
+    const updatedTasks = tasks.filter((task: any) => task.id !== id);
+    setTasks(updatedTasks);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <TasksList
+        tasks={tasks}
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EBEBEB",
+  },
+});
